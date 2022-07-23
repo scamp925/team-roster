676ac30f1c332175f83b7b6105709a788635b67d
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-23 14:13:41 -0500

    For the form, changed the input order to match what I have on figma

--- components/PlayerForm.js
+++ components/PlayerForm.js
@@ -45,14 +45,14 @@ function PlayerForm({ obj }) {
       <FloatingLabel controlId="floatingInput1" label="Full Name" className="mb-3">
         <Form.Control type="text" placeholder="Ex: Frederico Chiesa" name="name" value={formInput.name} onChange={handleChange} required />
       </FloatingLabel>
-      <FloatingLabel controlId="floatingInput2" label="Image URL" className="mb-3">
-        <Form.Control type="url" placeholder="Enter image url" name="imageUrl" value={formInput.imageUrl} onChange={handleChange} required />
-      </FloatingLabel>
       <FloatingLabel controlId="floatingInput3" label="Position" className="mb-3">
         <Form.Control type="text" placeholder="Ex: Midfielder" name="position" value={formInput.position} onChange={handleChange} required />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput4" label="Jersey Number" className="mb-3">
         <Form.Control type="text" placeholder="Ex: 14" name="jerseyNumber" value={formInput.jerseyNumber} onChange={handleChange} required />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingInput2" label="Image URL" className="mb-3">
+        <Form.Control type="url" placeholder="Enter image url" name="imageUrl" value={formInput.imageUrl} onChange={handleChange} required />
       </FloatingLabel>
       <Button type="submit">{obj?.firebaseKey ? 'Update' : 'Add'} Player</Button>
     </Form>
