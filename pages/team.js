9e7e538c6dcf41b2fbccb77b27758802f2cdf5ff
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-23 10:44:26 -0500

    Player cards render once Gli Azzurri is clicked on the navbar. The route changes to /team as well

--- pages/team.js
+++ pages/team.js
@@ -1,9 +1,31 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import getPlayers from '../api/playerData';
+import PlayerCards from '../components/PlayerCards';
+import { useAuth } from '../utils/context/authContext';
 
 export default function TeamRoster() {
+  const [players, setPlayers] = useState([]);
+  const { user } = useAuth();
+
+  const getTeamRoster = () => {
+    getPlayers(user.uid).then(setPlayers);
+  };
+
+  useEffect(() => {
+    getTeamRoster();
+  }, []);
+
   return (
     <div>
-      <h1>Team</h1>
+      <header>
+        <h1>Team</h1>
+      </header>
+      <div>
+        {players.map((player) => (
+          <PlayerCards key={player.firebaseKey} playerObj={player} />
+        ))}
+      </div>
     </div>
   );
 }
