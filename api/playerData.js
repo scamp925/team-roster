830569aad76579b0f8103ca2bd0e41e61db6dc74
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-23 10:39:16 -0500

    Wrote code for player cards to render on the landing page. They are there

--- api/playerData.js
+++ api/playerData.js
@@ -5,13 +5,14 @@ const dbUrl = clientCredentials.databaseURL;
 
 // GET ALL THE PLAYERS BY UID
 const getPlayers = (uid) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/players.json?orderBy="uid"&equalTo="${uid}"`).then((response) => {
-    if (response.data) {
-      resolve(Object.values(response.data));
-    } else {
-      resolve([]);
-    }
-  })
+  axios.get(`${dbUrl}/players.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
     .catch(reject);
 });
 
